438efe603703d8ab7efe0ae6008bdabae751e0cc
Hubert <kiszkahub@gmail.com>
2025-04-07 00:51:36 +0200

    Before using websockets

--- frontend/src/app/mainPage/existing-document/existing-document.component.ts
+++ frontend/src/app/mainPage/existing-document/existing-document.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Document} from '../../services/document.service';
+import {CommonModule} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-existing-document',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './existing-document.component.html',
   styleUrl: './existing-document.component.css'
 })
 export class ExistingDocumentComponent {
-
+  @Input() document!: Document;
+  constructor(private router: Router){}
+  openDocument(): void{
+    this.router.navigate(['/document',this.document._id])
+  }
 }
--- frontend/src/app/mainPage/new-document/new-document.component.ts
+++ frontend/src/app/mainPage/new-document/new-document.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ExistingDocumentComponent} from '../existing-document/existing-document.component';
-import {DocumentService} from '../../services/document.service';
+import {Document, DocumentService} from '../../services/document.service';
 import {Router} from '@angular/router';
 
 @Component({
@@ -11,12 +11,26 @@ import {Router} from '@angular/router';
   templateUrl: './new-document.component.html',
   styleUrl: './new-document.component.css'
 })
-export class NewDocumentComponent {
+export class NewDocumentComponent implements OnInit{
+  documents: Document[] = [];
   constructor(private documentService: DocumentService, private router: Router){}
+  ngOnInit() {
+    this.loadDocuments();
+  }
+
+  loadDocuments(): void{
+    this.documentService.getUserDocuments().subscribe({
+      next: data =>{
+        this.documents = data;
+      },
+      error: err => {
+        console.error(err);
+      }
+    })
+  }
   createNewDocument(){
     this.documentService.createDocument().subscribe({
       next: newDoc => {
-        console.log(newDoc);
         this.router.navigate(['/document',newDoc._id])
       },
       error: err => {
