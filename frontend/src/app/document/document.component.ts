fbe8ec6908bc8aff2cf959e977dbe6bed7e5e614
Hubert <kiszkahub@gmail.com>
2025-04-06 22:16:23 +0200

    saving docs to database works now

--- frontend/src/app/document/document.component.ts
+++ frontend/src/app/document/document.component.ts
@@ -11,45 +11,57 @@ import {DocumentService} from '../services/document.service';
   templateUrl: './document.component.html',
   styleUrl: './document.component.css'
 })
-export class DocumentComponent implements OnInit, OnDestroy{
+export class DocumentComponent implements OnInit, OnDestroy, AfterViewInit{
   documentId: string = '';
   editorContent: string = '';
   titleControl = new FormControl('Nowy dokument');
+  @ViewChild(QuillEditorComponent) editor!: QuillEditorComponent;
   private contentChangeSub: Subscription | undefined;
   private titleChangeSub: Subscription | undefined;
   private saveInProgress = false;
+  private editorChangeSub : Subscription | undefined
   constructor(private route: ActivatedRoute, private documentService: DocumentService) {}
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.documentId = params['id'];
       if(this.documentId) this.loadDocument();
     });
-    this.contentChangeSub = new FormControl(this.editorContent)
-      .valueChanges
-      .pipe(debounceTime(2000))
-      .subscribe(()=>{
-        if(this.documentId && !this.saveInProgress) this.saveDocument();
-      });
     this.titleChangeSub = this.titleControl.valueChanges
       .pipe(debounceTime(2000))
       .subscribe(()=>{
         if(this.documentId && !this.saveInProgress) this.saveDocument();
       })
   }
-  ngOnDestroy() {
-    if(this.contentChangeSub) this.contentChangeSub.unsubscribe();
-    if(this.titleChangeSub) this.titleChangeSub.unsubscribe();
+  ngAfterViewInit(){
+    this.editorChangeSub = this.editor.onContentChanged
+      .pipe(debounceTime(1000))
+      .subscribe((change: any) =>{
+        if(change.source === 'user'){
+          this.editorContent = this.editor.quillEditor.root.innerHTML;
+          if(this.documentId && !this.saveInProgress){
+            this.saveDocument();
+          }
+        }
+      })
   }
-  loadDocument(){
+  ngOnDestroy() {
+    if (this.contentChangeSub) this.contentChangeSub.unsubscribe();
+    if (this.titleChangeSub) this.titleChangeSub.unsubscribe();
+    if (this.editorChangeSub) this.editorChangeSub.unsubscribe();
+  }
+  loadDocument() {
     this.documentService.getDocument(this.documentId).subscribe(
       document => {
         this.editorContent = document.content;
-        this.titleControl.setValue(document.title);
+        this.titleControl.setValue(document.title, { emitEvent: false });
+        if (this.editor && this.editor.quillEditor) {
+          this.editor.quillEditor.clipboard.dangerouslyPasteHTML(0, document.content);
+        }
       },
-      err =>{
+      err => {
         console.error(err);
       }
-    )
+    );
   }
   saveDocument(){
     this.saveInProgress = true;
